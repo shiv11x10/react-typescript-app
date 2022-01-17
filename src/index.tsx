eaed5d8850291cc4db9811880f127848adbf2cde
shiv11x10 <shivam96.sa@gmail.com>
2022-01-18 00:28:19 +0530

    first commitImplementing In-browser bundling

---
+++ src/index.tsx
@@ -0,0 +1,58 @@
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+
+const App = () => {
+  const ref = useRef<any>(); //to use data inside startService in onClick
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  //start the esbuild service
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  // initialize esbuild only on mount of component once
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    // const result = await ref.current.transform(input, {
+    //   loader: "jsx",
+    //   target: "es2015",
+    // });
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    console.log(result);
+
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
